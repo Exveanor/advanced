50f922ab0632f949cec75b23551b7bcc9f346b27
Exveanor <eviexadore@gmail.com>
2023-05-20 20:39:13 +0300

    Some problems from today

--- objects advanced/01CalorieObject.js
+++ objects advanced/01CalorieObject.js
@@ -1,5 +1,14 @@
 function calorieObject (array) {
+    const result = {};
 
+    for (let i = 0; i < array.length; i+=2) {
+        const element = array[i];
+
+        result[array[i]] = Number([array[i+1]]);
+        
+    }
+    return result;
 }
-calorieObject (['Yoghurt', '48', 'Rise', '138',
-'Apple', '52'] )
+calorieObject (['Yoghurt', '48', 'Rise', '138','Apple', '52']);
+
+//{ Yoghurt: 48, Rise: 138, Apple: 52 }
---
+++ objects advanced/02constitutionCrew.js
@@ -0,0 +1,14 @@
+function constitutionCrew (input) {
+    const obj = {... input};
+
+    if(obj.dizziness){
+        obj.levelOfHydrated += 0.1 * obj.weight * obj.experience;
+        obj.dizziness = false;
+        return obj;
+    }
+    return obj;
+}
+constitutionCrew({ weight: 80,
+    experience: 1,
+    levelOfHydrated: 0,
+    dizziness: true })
