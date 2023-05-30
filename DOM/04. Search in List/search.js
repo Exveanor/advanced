94b61be724aa7327a5b26ec06d6c6f337d5ed37d
Exveanor <eviexadore@gmail.com>
2023-05-30 04:57:33 +0300

    .

---
+++ DOM/04. Search in List/search.js
@@ -0,0 +1,23 @@
+function search() {
+   const listItems = document.querySelectorAll("#towns li");
+   const inputValue = document.querySelector("input").value;
+   const result = document.getElementById("result");
+
+   let sum = 0;
+
+   for (const li of listItems) {
+      const inputToLower = inputValue.toLowerCase();
+      const liTextToLower = li.textContent.toLowerCase();
+
+      if(liTextToLower.includes(inputToLower)) {
+         li.style.fontWeight = "bold";
+         li.style.textDecoration = "underline";
+         sum++;
+      } else {
+         li.style.fontWeight = "";
+         li.style.textDecoration = ""
+      }
+   }
+
+   result.textContent = `${sum} matches found`;
+}
