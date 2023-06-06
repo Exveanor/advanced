71b95e1acd4728ded7441cb5d09b1aa93da2a793
Exveanor <eviexadore@gmail.com>
2023-06-07 00:11:18 +0300

    .

---
+++ DOM Manipulations and Events/04. Mouse-Gradient/app.js
@@ -0,0 +1,18 @@
+function attachGradientEvents() {
+    let gradient = document.getElementById("gradient");
+    gradient.addEventListener("mousemove", hoverHandler);
+    let resultDiv = document.getElementById("result");
+    gradient.addEventListener("mouseout", clearHandler)
+
+    function hoverHandler(e) {
+        let offsetX = e.offsetX;
+        let element = e.target;
+        let elementX = element.clientWidth;
+        let xPercent = Math.trunc((offsetX / elementX) * 100);
+        resultDiv.textContent = `${xPercent}%`;
+    }
+
+    function clearHandler() {
+        resultDiv.textContent = "";
+    }
+}
