52382678fa7dc76226b1fe385e4626bdcb151d92
Exveanor <eviexadore@gmail.com>
2023-06-11 23:55:42 +0300

    .

---
+++ advanced functions/functionalSum.js
@@ -0,0 +1,14 @@
+function add(num) {
+    let sum = 0;
+    sum += num;
+
+    function calc(anotherNum) {
+        sum += anotherNum;
+        return calc;
+    };
+
+    calc.toString = () => sum;
+    return calc;
+}
+
+console.log(add(1)(6)(-3)(5));
