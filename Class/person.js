485a1a773d14f78b55ef723c6b566cc9b5b6517f
Exveanor <eviexadore@gmail.com>
2023-06-15 20:26:33 +0300

    .

---
+++ Class/person.js
@@ -0,0 +1,16 @@
+class Person {
+    constructor(fName, lName, age, email) {
+        this.firstName = fName;
+        this.lastName = lName;
+        this.age = age;
+        this.email = email;
+    }
+
+    toString() {
+        return `${this.firstName} ${this.lastName} (age: ${this.age}, email: ${this.email})}`
+    }
+}
+
+let person = new Person("Anna", "Simson", 22, "anna@yahoo.com");
+console.log(person);
+console.log(person.toString());
