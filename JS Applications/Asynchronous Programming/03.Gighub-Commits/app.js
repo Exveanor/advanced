4d2ce31e5c12211ab95701e3c8769cb4c4a63356
Exveanor <eviexadore@gmail.com>
2023-07-08 01:44:52 +0300

    Update app.js

--- JS Applications/Asynchronous Programming/03.Gighub-Commits/app.js
+++ JS Applications/Asynchronous Programming/03.Gighub-Commits/app.js
@@ -1,4 +1,13 @@
-function loadCommits() {
-    // Try it with Fetch API
-    console.log('TODO...');
+async function loadCommits() {
+    let usernameInput = document.getElementById("username");
+    let repoInput = document.getElementById("repo");
+    let username = usernameInput.value;
+    let repo = repoInput.value;
+
+    let url = `https://api.github.com/repos/${username}/${repo}/commits`;
+
+    let commits = await fetch(url)
+        .then(response => response.json());
+  
+    console.log(commits);
 }
