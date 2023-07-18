c06ed04155a19ec96622e7ac2f8d05df16056f02
Exveanor <eviexadore@gmail.com>
2023-07-18 07:41:57 +0300

    .

--- JS Applications/Asynchronous Programming/01.Bus-Stop/app.js
+++ JS Applications/Asynchronous Programming/01.Bus-Stop/app.js
@@ -1,3 +1,25 @@
-function getInfo() {
-    console.log("TODO...");
+async function getInfo() {
+    // get data from http://localhost:3030/jsonstore/bus/businfo/:busId
+    // parse
+    // error handle 
+    // add li elements per bus -> "Bus {busID} arrives in {time} minutes"
+
+    // get elements 
+    const stopId = document.getElementById("stopId").value;
+    const busesUl = document.getElementById("buses");
+    busesUl.innerHTML = "";
+    try {
+    const response = await fetch(`http://localhost:3030/jsonstore/bus/businfo/${stopId}`);
+    const data = await response.json();
+
+    document.getElementById("stopName").textContent = data.name;
+
+    Object.entries(data.buses).forEach(([busId, time]) => {
+        const liElement = document.createElement("li");
+        liElement.textContent = `Bus ${busId} arrives in ${time} minutes`;
+        busesUl.appendChild(liElement);
+    });
+} catch (err) {
+    document.getElementById("stopName").textContent = "Error";
+}
 }
