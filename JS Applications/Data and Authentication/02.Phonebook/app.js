3a40989852973873382e03605af18a35cb298b95
Exveanor <eviexadore@gmail.com>
2023-07-24 16:40:35 +0300

    .

---
+++ JS Applications/Data and Authentication/02.Phonebook/app.js
@@ -0,0 +1,26 @@
+// load phonebook -> use fetch (get)
+// capture elements
+// delete entries -> use fetch (delete)
+// create entry - use fetch (post)
+
+const baseURL = "http://localhost:3030/jsonstore/phonebook"
+
+function attachEvents() {
+    document.getElementById("btnLoad").addEventListener("click", loadPhoneBook);
+
+}
+
+attachEvents();
+
+ async function loadPhoneBook() {
+    const response = await fetch(baseURL);
+    const data = await response.json();
+    const phoneBookElement = document.getElementById("phonebook");
+    phoneBookElement.innerHTML = "";
+    
+    Object.values(data).forEach(x => {
+        const liElement = document.createElement("li");
+        liElement.textContent = `${x.person}: ${x.phone}`;
+        phoneBookElement.appendChild(liElement);
+    });
+}
