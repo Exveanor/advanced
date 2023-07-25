d2b807cc206a4d97f98e4f2536145c8ca8fd1197
Exveanor <eviexadore@gmail.com>
2023-07-25 04:55:09 +0300

    Update app.js

--- JS Applications/Data and Authentication/02.Phonebook/app.js
+++ JS Applications/Data and Authentication/02.Phonebook/app.js
@@ -8,21 +8,22 @@ const baseURL = "http://localhost:3030/jsonstore/phonebook"
 function attachEvents() {
     document.getElementById("btnLoad").addEventListener("click", loadPhoneBook);
     document.getElementById("phonebook").addEventListener("click", deletePhone);
+    document.getElementById("btnCreate").addEventListener("click", createPhone);
 
 }
 
 attachEvents();
 
- async function loadPhoneBook() {
+async function loadPhoneBook() {
     const response = await fetch(baseURL);
     const data = await response.json();
     const phoneBookElement = document.getElementById("phonebook");
     phoneBookElement.innerHTML = "";
-    
+
     Object.values(data).forEach(x => {
         const liElement = document.createElement("li");
         liElement.textContent = `${x.person}: ${x.phone}`;
-        
+
         // create delete button 
         const deleteButtonElement = document.createElement("button");
         deleteButtonElement.setAttribute("id", x._id);
@@ -41,5 +42,32 @@ async function deletePhone(e) {
     await fetch(`${baseURL}/${currentPhoneId}`, {
         method: "DELETE",
     });
+    loadPhoneBook();
+}
+
+async function createPhone() {
+    const person = document.getElementById("person").value.trim();
+    const phone = document.getElementById("phone").value.trim();
+
+    if (!person.value || !phone.value) {
+        alert("Person and Phone fields are required!");
+    };
+
+    const requestData = JSON.stringify({
+        person,
+        phone
+    });
+
+    await fetch(baseURL, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: requestData,
+    });
+
+    document.getElementById("person").value = "";
+    document.getElementById("phone").value = "";
+
     loadPhoneBook();
 }
