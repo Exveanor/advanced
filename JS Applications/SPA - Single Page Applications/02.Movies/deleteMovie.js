80101a2c724dc1e2987a09a5844fa54c7b47272d
Exveanor <eviexadore@gmail.com>
2023-07-27 16:49:29 +0300

    Update deleteMovie.js

--- JS Applications/SPA - Single Page Applications/02.Movies/deleteMovie.js
+++ JS Applications/SPA - Single Page Applications/02.Movies/deleteMovie.js
@@ -1,19 +1,18 @@
-import { request } from './utils.js'
-import { showSection, setupHome } from './app.js'
+import { request } from "./utils.js";
+import { showSection, setupHome } from "./app.js";
 
-const url = 'http://localhost:3030/data/movies'
+const url = "http://localhost:3030/data/movies";
 
 export async function deleteMovie(e, movieId) {
-    try {
-        await request(url + '/' + movieId, {
-            method: 'DELETE',
-            headers: { "X-Authorization": sessionStorage.getItem('authToken') }
-        })
+  try {
+    await request(url + "/" + movieId, {
+      method: "DELETE",
+      headers: { "X-Authorization": sessionStorage.getItem("authToken") },
+    });
 
-        showSection('home-page')
-        setupHome()
-
-    } catch (err) {
-        console.log(err);
-    }
+    showSection("home-page");
+    setupHome();
+  } catch (err) {
+    console.log(err);
+  }
 }
